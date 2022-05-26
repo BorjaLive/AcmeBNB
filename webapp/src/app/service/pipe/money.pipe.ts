40309228ac6532ec76c53cdc6e5c3ab2dad9e445
BorjaLive <borjainlive@gmail.com>
2022-05-26 20:55:56 +0200

    No usar traducciones en las pipes porque no se actualizan al cambiar de idioma

--- webapp/src/app/service/pipe/money.pipe.ts
+++ webapp/src/app/service/pipe/money.pipe.ts
@@ -11,9 +11,7 @@ export class MoneyPipe implements PipeTransform {
   ) { }
 
   transform(value: number, ...args: string[]): unknown {
-    let simbol = "€";
-    if(args[0] && args[0] === "rate") simbol = `€/${this.translator.instant("SHARED.DAY")}`;
-    return `${(Math.round(value * 100) / 100).toFixed(2)} ${simbol}`;
+    return `${(Math.round(value * 100) / 100).toFixed(2)} €`;
   }
 
 }
